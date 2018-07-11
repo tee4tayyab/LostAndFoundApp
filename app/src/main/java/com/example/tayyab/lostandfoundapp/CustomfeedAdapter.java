77223b64e5657ea4c6827d94fd667be8e321a43a
Tayyab <tayyabrasheed330@gmail.com>
2018-07-11 21:31:05 +0500

    Lost and Found Initial Commit.

--- app/src/main/java/com/example/tayyab/lostandfoundapp/CustomfeedAdapter.java
+++ app/src/main/java/com/example/tayyab/lostandfoundapp/CustomfeedAdapter.java
@@ -1,6 +1,7 @@
 package com.example.tayyab.lostandfoundapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -39,6 +40,11 @@ public class CustomfeedAdapter extends RecyclerView.Adapter<CustomfeedAdapter.Sa
     public void onBindViewHolder(@NonNull SampleViewHolder holder, int position) {
 
         Customfeeditem model = SampleModelList.get(position);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
         // holder.txtUsername.setText("asd");
         //    holder.txtDescription.setText("asdsadsa");
@@ -68,8 +74,12 @@ public class CustomfeedAdapter extends RecyclerView.Adapter<CustomfeedAdapter.Sa
         private ImageView imgviewUserImageUrl;
         private ImageView imgviewPostImageUrl;
 
+
         public SampleViewHolder(View itemView) {
             super(itemView);
+            Context context = itemView.getContext();
+            Intent intent = new Intent(context,CommentsPost.class);
+            context.startActivity(intent);
 
             txtUsername = itemView.findViewById(R.id.txt_ProfileName);
 
